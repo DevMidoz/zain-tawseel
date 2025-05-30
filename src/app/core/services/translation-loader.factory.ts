4f0332d98435eec6e9f2584e9a7fd02e9a932095
DevMidoz <dev.ahmedyasser@gmail.com>
2025-05-30 06:14:07 +0300

    Fix Language isuue

--- src/app/core/services/translation-loader.factory.ts
+++ src/app/core/services/translation-loader.factory.ts
@@ -3,5 +3,5 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
